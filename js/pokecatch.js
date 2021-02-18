3e0bb4b37c6b081aced7fdde55b95cd8c457d479
samuelbrinke <samuel.brinkeback@hotmail.com>
2021-02-18 17:17:47 +0100

    Added a start button, getting random pokemon.

--- js/pokecatch.js
+++ js/pokecatch.js
@@ -2,4 +2,36 @@
 function getRandomArbitrary(max) {
   return Math.floor(Math.random() * max) + 1;
 }
-//console.log(getRandomArbitrary(99));
+
+let randomNum = getRandomArbitrary(99);
+
+let pokeImg = document.querySelector('.poke-img');
+let pokeName = document.querySelector('.poke-name');
+function getPokemons() {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      pokeImg.src = data.sprites.versions['generation-v']['black-white']['animated'].front_default;
+      pokeName.innerText = data.name;
+      console.log(pokeImg.src)
+    });
+}
+getPokemons();
+
+let startBtn = document.querySelector('.start-btn');
+startBtn.addEventListener('click', function() {
+  let pokeBattleContent = document.querySelector('.pokebattle-content')
+  let pokeBattle = document.querySelector('.pokebattle-wrapper');
+  let battleAudio = document.createElement('audio');
+  let battleSrc = document.createElement('source');
+
+  startBtn.classList.add('hide');
+  pokeBattleContent.classList.remove('hide');
+
+  battleAudio.setAttribute('autoplay','');
+  battleSrc.src = '../sounds/battle.mp3'
+  battleAudio.volume = 0.02;
+  pokeBattle.appendChild(battleAudio);
+  battleAudio.appendChild(battleSrc);
+})

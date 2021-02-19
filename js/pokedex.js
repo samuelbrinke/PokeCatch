ab997567e0c4ed67079d8098c5e666d4de7a283e
Jonatan Eriksson <jonatan-eriksson@users.noreply.github.com>
2021-02-19 17:24:51 +0100

    Refactor getPokemons

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,41 +1,54 @@
-function getPokemons() {
-  const pokedexCards = document.querySelector('.pokedex-cards');
-  fetch('https://pokeapi.co/api/v2/pokemon')
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
+const pokedex = document.querySelector('.pokedex-cards');
 
-      const pokemons = data.results;
-      for (let key in pokemons) {
-        const pokemon = pokemons[key];
-
-        const card = document.createElement('div');
-        const img = document.createElement('img');
-        const content = document.createElement('div');
-        const title = document.createElement('h3');
-        const text = document.createElement('p');
-
-        card.className = 'card';
-        img.className = 'card-img';
-        content.className = 'card-content';
-        title.className = 'card-title';
-        text.className = 'card-text';
-
-        content.append(title, text);
-        card.append(img, content);
-
-        title.textContent = pokemon.name;
-
-        fetch(pokemon.url)
-          .then((res) => res.json())
-          .then((data) => {
-            img.src = data.sprites.front_default;
-            data.types.forEach((type) => (text.textContent += type.type.name + ' '));
-          });
-
-        pokedexCards.append(card);
-      }
-    });
+async function getPokemons(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`An error occured: ${response.status}`);
+  return await response.json();
 }
 
-getPokemons();
+function createPokemonCards(pokemons) {
+  pokemons.forEach((pokemon) => {
+    const card = document.createElement('div');
+    const img = document.createElement('img');
+    const content = document.createElement('div');
+    const title = document.createElement('h3');
+    const text = document.createElement('p');
+
+    card.className = 'card';
+    img.className = 'card-img';
+    content.className = 'card-content';
+    title.className = 'card-title';
+    text.className = 'card-text';
+
+    content.append(title, text);
+    card.append(img, content);
+
+    title.textContent = pokemon.name;
+
+    img.src = pokemon.sprites.front_default;
+    pokemon.types.forEach((item) => {
+      const type = item.type.name;
+      text.innerHTML = `<span class="pokemon-type ${type}">${type}</span>`;
+    });
+
+    pokedex.append(card);
+  });
+}
+
+async function loadPokemons() {
+  if (apiUrl === null) return;
+  const response = await getPokemons(apiUrl);
+  console.log(response);
+
+  const pokemons = await Promise.all(
+    response.results?.map(async (pokemon) => {
+      const url = new URL(pokemon.url);
+      return await getPokemons(url);
+    })
+  );
+
+  createPokemonCards(pokemons);
+}
+
+let apiUrl = new URL('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
+loadPokemons();

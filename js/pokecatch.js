36bc7a6df84a68908fae60afc5a38c3356d33287
samuelbrinke <samuel.brinkeback@hotmail.com>
2021-02-19 11:12:11 +0100

    Testing localstorage and if pokémon is caught.

--- js/pokecatch.js
+++ js/pokecatch.js
@@ -1,26 +1,40 @@
 //Random number generator
-function getRandomArbitrary(max) {
+function getRanNum(max) {
   return Math.floor(Math.random() * max) + 1;
 }
 
-let randomNum = getRandomArbitrary(99);
+let randomNum = getRanNum(99);
 
 let pokeImg = document.querySelector('.poke-img');
 let pokeName = document.querySelector('.poke-name');
-function getPokemons() {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      pokeImg.src = data.sprites.versions['generation-v']['black-white']['animated'].front_default;
-      pokeName.innerText = data.name;
-      console.log(pokeImg.src)
-    });
-}
-getPokemons();
 
 let startBtn = document.querySelector('.start-btn');
 startBtn.addEventListener('click', function() {
+  async function getPokemon() {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`);
+    let data = await response.json();
+    pokeImg.src = data.sprites.versions['generation-v']['black-white']['animated'].front_default;
+    pokeName.innerText = data.name;
+    console.log(pokeImg.src)
+
+    let getPokeName = data.name;
+
+    function getRanChance(max) {
+      return Math.floor(Math.random() * max) + 1;
+    }
+    let catched = getRanChance(2);
+    console.log(getRanChance(2));
+    
+    if(catched == 1) {
+      localStorage.setItem('PokeName', getPokeName);
+      console.log("You catched the pokémon!");
+      //console.log(localStorage());
+    } else {
+      console.log("Pokémon ran away!")
+    }
+  };
+  getPokemon();
+
   let pokeBattleContent = document.querySelector('.pokebattle-content')
   let pokeBattle = document.querySelector('.pokebattle-wrapper');
   let battleAudio = document.createElement('audio');
@@ -34,4 +48,5 @@ startBtn.addEventListener('click', function() {
   battleAudio.volume = 0.02;
   pokeBattle.appendChild(battleAudio);
   battleAudio.appendChild(battleSrc);
+
 })

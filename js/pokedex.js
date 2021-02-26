b2974e260f3dd8cb2ca12200c1ab9d1c51b3a7d9
Jonatan Eriksson <jonatan-eriksson@users.noreply.github.com>
2021-02-27 00:40:13 +0100

    Add a modal func to view pokemon stats on toggle

--- js/pokedex.js
+++ js/pokedex.js
@@ -2,16 +2,20 @@ import { fetchPokemons, fetchPokemon, resetNextUrl } from './fetch.js';
 
 const pokedex = document.querySelector('.pokedex-cards');
 
+let loading = false;
 loadPokemons();
 
+// Load pokemons
 async function loadPokemons() {
+  if (loading === true) return;
   toggleLoader(true);
+
   const pokemons = await fetchPokemons();
   pokemons?.forEach((pokemon) => createPokemonCard(pokemon));
   toggleLoader();
 }
 
-// Auto load more at bottom if enabled
+// Load more at bottom if enabled
 function enableScroll() {
   window.onscroll = async () => {
     const height = document.documentElement.scrollHeight;
@@ -59,10 +63,12 @@ function toggleLoadButton(show = false) {
 }
 
 function toggleLoader(show = false) {
+  loading = show;
   if (show === true) document.querySelector('.loader').classList.remove('hide');
   else document.querySelector('.loader').classList.add('hide');
 }
 
+// Pokedex card
 function createPokemonCard(pokemon) {
   if (pokemon == null) return;
   // pokemons.forEach((pokemon) => {
@@ -73,28 +79,89 @@ function createPokemonCard(pokemon) {
   const text = document.createElement('p');
 
   card.className = 'card';
+  card.dataset.pokemonId = pokemon.id;
+
   img.className = 'card-img';
-  content.className = 'card-content';
-  title.className = 'card-title';
-  text.className = 'card-text';
-
-  content.append(title, text);
-  card.append(img, content);
-
-  title.textContent = pokemon.name;
-
   img.src =
-    pokemon.sprites.other.dream_world.front_default ??
-    pokemon.sprites.other['official-artwork'].front_default ??
-    '#';
+    // pokemon.sprites.other.dream_world.front_default ??
+    pokemon.sprites.other['official-artwork'].front_default ?? '#';
   img.alt = pokemon.name;
   img.loading = 'lazy';
+
+  content.className = 'card-content';
+
+  title.className = 'card-title';
+  title.textContent = pokemon.name;
+
+  text.className = 'card-text';
 
   pokemon.types.forEach((item) => {
     const type = item.type.name;
     text.innerHTML += `<span class="pokemon-type ${type}">${type}</span>`;
   });
 
+  content.append(title, text);
+  card.append(img, content);
+
   pokedex.append(card);
   // });
+}
+
+// Modal
+pokedex.addEventListener('click', (e) => {
+  if (e.target.classList.contains('card') && !e.target.classList.contains('not-collected')) {
+    const id = e.target.dataset.pokemonId;
+    showPokemonModal(id);
+  }
+});
+
+document.querySelector('.modal-btn').addEventListener('click', toggleModal);
+function toggleModal() {
+  document.querySelector('.modal').classList.toggle('hide');
+}
+
+async function showPokemonModal(pokemonId) {
+  console.log(pokemonId);
+  const pokemon = await fetchPokemon(pokemonId);
+
+  console.log(pokemon);
+  // Elements
+  const modal = document.querySelector('.modal');
+  const img = modal.querySelector('.modal-img');
+  const name = modal.querySelector('.pokemon-name');
+  const type = modal.querySelector('.type');
+  const height = modal.querySelector('.details-height');
+  const weight = modal.querySelector('.details-weight');
+  const stats = modal.querySelectorAll('.progress-done');
+
+  img.src =
+    // pokemon.sprites.other.dream_world.front_default ??
+    pokemon.sprites.other['official-artwork'].front_default ?? '#';
+
+  name.textContent = pokemon.name;
+
+  height.textContent = `${pokemon.height / 10} M`;
+  weight.textContent = `${pokemon.weight / 10} KG`;
+
+  stats[0].style = `width: ${(pokemon.stats[0].base_stat / 250) * 100}%`;
+  stats[0].textContent = pokemon.stats[0].base_stat;
+
+  stats[1].style = `width: ${(pokemon.stats[1].base_stat / 250) * 100}%`;
+  stats[1].textContent = pokemon.stats[1].base_stat;
+
+  stats[2].style = `width: ${(pokemon.stats[2].base_stat / 250) * 100}%`;
+  stats[2].textContent = pokemon.stats[2].base_stat;
+
+  stats[3].style = `width: ${(pokemon.stats[5].base_stat / 250) * 100}%`;
+  stats[3].textContent = pokemon.stats[5].base_stat;
+
+  stats[4].style = `width: ${(pokemon.base_experience / 500) * 100}%`;
+  stats[4].textContent = pokemon.base_experience;
+
+  // console.log(progressList);
+  type.innerHTML = pokemon.types
+    .map((type) => `<span class="pokemon-type ${type.type.name}">${type.type.name}</span>`)
+    .join(' ');
+
+  toggleModal();
 }
